74d867a400cf0f479cc0151c517bd283acc42e9b
texascake soft <budalblimbing@gmail.com>
2024-11-22 09:19:24 +0000

    Revert "Welcome Retro Music Player!"
    This reverts commit b9333834d9b64e449f8242db6f982bb305b0c07c.

--- prebuilts.mk
+++ prebuilts.mk
@@ -3,5 +3,4 @@ PRODUCT_PACKAGES += \
     SoundPickerPrebuilt \
     GalleryGoPrebuilt \
     CalendarGooglePrebuilt \
-    CalculatorGooglePrebuilt \
-    RetroMusicPlayerPrebuilt
+    CalculatorGooglePrebuilt
